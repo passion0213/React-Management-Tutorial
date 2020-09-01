e957e084b4f3f4ccb283f3f9523a968d471b6776
Fstar0213 <Devsmith0213@gmail.com>
2020-09-01 15:40:13 +0800

    Create Customer Database Table

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+
+connection.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            "id": 1,
-            "image": "https://placeimg.com/64/64/1",
-            "name": "Smith",
-            "birthday": "19920219",
-            "gender": "male",
-            "job": "student"
-        },
-        {
-            "id": 2,
-            "image": "https://placeimg.com/64/64/2",
-            "name": "Ana",
-            "birthday": "19850219",
-            "gender": "female",
-            "job": "doctor"
-        },
-        {
-            "id": 3,
-            "image": "https://placeimg.com/64/64/3",
-            "name": "John",
-            "birthday": "20050219",
-            "gender": "male",
-            "job": "student"
-        },
-    ]);
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
